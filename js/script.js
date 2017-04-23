0106ad9f7a8a33977f0edaedd8217bed2cd12a03
3048 <alurchik29@gmail.com>
2017-04-23 17:56:01 +0300

    5

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log("test");
+$(function () {
+
+
+	/* Transparent menu on scroll */
+
+
+	var okno = $(window);
+	var menu = $('#menu');
+
+	if (okno.scrollTop() >= 50) {
+		menu.addClass('scroll');
+	}
+
+	okno.scroll(function () {
+		if (okno.scrollTop() >= 100) {
+			menu.addClass('scroll');
+		} else {
+			menu.removeClass('scroll');
+
+		}
+	});
+
+
+	/* Smooth scroll*/
+
+	$(document).on('click', 'a[href^="#"]', function (event) {
+		event.preventDefault();
+
+		var menuHeight = $('#menu').height();
+
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top - menuHeight
+		}, 500);
+	});
+});
